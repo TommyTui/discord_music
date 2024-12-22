4dafbbf9ed5978f78f48259177536593eda18ebb
Thomas Cui <zc246@cornell.edu>
2024-12-21 22:51:38 -0500

    proj

---
+++ db.py
@@ -0,0 +1,28 @@
+from peewee import SqliteDatabase, Model, CharField, IntegerField, fn
+
+db = SqliteDatabase('discord.db')
+
+
+class Music(Model):
+    url = CharField()
+    type = CharField()
+    pid = IntegerField(null=True)
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([Music])
+
+
+def random_music(count=5):
+    return Music.select().order_by(fn.Random()).limit(count)
+
+
+def insert(url, type, pid=None):
+    Music.create(url=url, type=type, pid=pid)
+
+
+def delete(url, pid=None):
+    Music.delete().where(Music.url == url, Music.pid == pid).execute()
