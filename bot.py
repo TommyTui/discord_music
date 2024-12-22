2bac0abfbbfb45fcc61a936fe5b5e7f3d54fcb64
Thomas Cui <zc246@cornell.edu>
2024-12-21 22:51:38 -0500

    proj

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import random
+import threading
 import traceback
 
 import discord
@@ -10,6 +11,8 @@ from discord import Intents
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from db import *
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 intent = Intents.default()
@@ -18,12 +21,13 @@ intent.voice_states = True
 intent.messages = True
 queue = asyncio.Queue()
 bot = commands.Bot(command_prefix='!', intents=intent)
+cv = threading.Condition()
 
 
 @bot.tree.command(description="摸摸")
 async def pat(interaction: discord.Interaction):
     r = random.random()
-    if r<0.97:
+    if r < 0.95:
         await interaction.response.send_message("喵~")
     else:
         await interaction.response.send_message("摸摸你的")
@@ -32,21 +36,16 @@ async def pat(interaction: discord.Interaction):
 @bot.tree.command(description="Leave the voice channel")
 async def leave(interaction: discord.Interaction):
     if interaction.guild.voice_client:
-        await interaction.guild.voice_client.disconnect(force=True)
+        await interaction.guild.voice_client.disconnect(force=False)
         await interaction.response.send_message("Disconnected")
+        while not queue.empty():
+            try:
+                queue.get_nowait()
+                queue.task_done()
+            except asyncio.QueueEmpty:
+                break
     else:
         await interaction.response.send_message("Not in any voice channel")
-
-
-def is_url(query) -> bool:
-    regex = re.compile(
-        r'^(?:http|ftp)s?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return re.match(regex, query) is not None
 
 
 @bot.tree.command(description="Add a music to the queue")
@@ -55,10 +54,12 @@ async def play(interaction: discord.Interaction, query: str):
     if not await ensure_voice(interaction):
         return
     try:
-        if is_url(query):
+        if query.startswith("http"):
             if 'bilibili' in query:
                 bv = query.split('/')[4]
-                await enqueue_bilibili(interaction, bv)
+                match = re.search(r'\?p=\d+', query)
+                pid = int(match.group(0).split('=')[1]) if match else None
+                await enqueue_bilibili(interaction, bv, pid)
             elif 'youtube' in query:
                 await enqueue_ytb(interaction, query, False)
             else:
@@ -71,32 +72,39 @@ async def play(interaction: discord.Interaction, query: str):
         await interaction.followup.send("出问题了")
 
 
-async def enqueue_bilibili(interaction: discord.Interaction, bv) -> None:
-    # await interaction.response.defer()
+async def enqueue_bilibili(interaction: discord.Interaction, bv, pid=None) -> None:
     v = video.Video(bvid=bv)
-    url = await v.get_download_url(page_index=0)
-    detector = video.VideoDownloadURLDataDetecter(url)
-    stream = detector.detect_best_streams(
-        audio_accepted_qualities=[AudioQuality._64K, AudioQuality._132K, AudioQuality._192K])
-    stream: List[AudioStreamDownloadURL] = [s for s in stream if isinstance(s, AudioStreamDownloadURL)]
-    if not stream:
-        await interaction.followup.send("找不到啊")
-        return
-    music = stream[0].url
-    i = await v.get_info()
-    data = Data.from_bili(i)
 
-    header = f'User-Agent: {HEADERS["User-Agent"]}\r\nReferer: {HEADERS["Referer"]}\r\n'
-    ffmpeg_options = {
-        'before_options': f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -headers "{header}"',
-        'options': '-vn'
-    }
-    await queue.put((interaction, data, discord.FFmpegPCMAudio(music, **ffmpeg_options)))
-    await interaction.followup.send(f"Added **{data.title}** to queue")
+    async def enqueue_one(data):
+        detector = video.VideoDownloadURLDataDetecter(data.url)
+        stream = detector.detect_best_streams(
+            audio_accepted_qualities=[AudioQuality._64K, AudioQuality._132K, AudioQuality._192K])
+        stream: List[AudioStreamDownloadURL] = [s for s in stream if isinstance(s, AudioStreamDownloadURL)]
+        if not stream:
+            await interaction.followup.send("找不到啊")
+            return
+
+        data.url = stream[0].url
+        await queue.put((interaction, data))
+        await interaction.followup.send(f"Added **{data.title}** to queue")
+
+    info = await v.get_info()
+    if pid is not None:
+        url = await v.get_download_url(page_index=pid)
+        await enqueue_one(Data.from_bili(info, url, False))
+    else:
+        pages = await v.get_pages()
+        if len(pages) > 1:
+            for page in await v.get_pages():
+                cid = page['cid']
+                url = await v.get_download_url(cid=cid)
+                await enqueue_one(Data.from_bili(page, url, True, info['pic']))
+        else:
+            url = await v.get_download_url(page_index=0)
+            await enqueue_one(Data.from_bili(info, url, False))
 
 
 async def enqueue_ytb(interaction: discord.Interaction, url, search) -> None:
-    # await interaction.response.defer()
     ytdl_format_options = {
         'format': 'bestaudio/best',
         'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
@@ -112,20 +120,16 @@ async def enqueue_ytb(interaction: discord.Interaction, url, search) -> None:
     }
     ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
     if search:
-        result = ytdl.extract_info(url=f"ytsearch:{url}", download=False)['entries']
-        result = [r for r in result if r['duration'] < 1800]
+        result = ytdl.extract_info(url=f"ytsearch:{url}, original mv, ost, high quality", download=False)['entries']
+        result = [r for r in result if r['duration'] < 60 * 60 * 3]
         if not result:
             await interaction.followup.send("找不到啊")
             return
         i = result[0]
     else:
         i = ytdl.extract_info(url=url, download=False)
-    data = Data.from_yt(i)
-    ffmpeg_options = {
-        'before_options': f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn'
-    }
-    await queue.put((interaction, data, discord.FFmpegPCMAudio(i['url'], **ffmpeg_options)))
+    data = Data.from_yt(i, i['url'])
+    await queue.put((interaction, data))
     await interaction.followup.send(f"Added **{data.title}** to queue")
 
 
@@ -143,30 +147,52 @@ def create_embed(interaction: discord.Interaction, data):
     return embed
 
 
+def play_cb(error):
+    with cv:
+        cv.notify()
+
+
 async def _play():
     while True:
-        try:
-            print("wait", flush=True)
-            interaction: discord.Interaction
-            interaction, data, music = await queue.get()
-            print("pop", flush=True)
-            await interaction.channel.send(embed=create_embed(interaction, data))
-            interaction.guild.voice_client.play(music)
-            while interaction.guild.voice_client and interaction.guild.voice_client.is_playing():
-                await asyncio.sleep(2)
-            if queue.empty() and interaction.guild.voice_client:
-                await interaction.guild.voice_client.disconnect(force=True)
-        except Exception as e:
-            traceback.print_exc()
+        print("wait", flush=True)
+        interaction: discord.Interaction
+        interaction, data = await queue.get()
+        print("pop", flush=True)
+        await interaction.channel.send(embed=create_embed(interaction, data))
+        music = get_bilibili(data) if data.type == 0 else get_youtube(data)
+        interaction.guild.voice_client.play(music, after=play_cb)
+        with cv:
+            while interaction.guild.voice_client.is_playing():
+                cv.wait()
+        if queue.empty() and interaction.guild.voice_client:
+            await interaction.guild.voice_client.disconnect(force=False)
+        queue.task_done()
+
+
+def get_bilibili(data):
+    header = f'User-Agent: {HEADERS["User-Agent"]}\r\nReferer: {HEADERS["Referer"]}\r\n'
+    ffmpeg_options = {
+        'before_options': f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -headers "{header}"',
+        'options': '-vn'
+    }
+    return discord.FFmpegPCMAudio(data.url, **ffmpeg_options)
+
+
+def get_youtube(data):
+    ffmpeg_options = {
+        'before_options': f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn'
+    }
+    return discord.FFmpegPCMAudio(data.url, **ffmpeg_options)
 
 
 async def ensure_voice(interaction: discord.Interaction):
     try:
         if interaction.user.voice:
-            if interaction.guild.voice_client:
-                await interaction.guild.voice_client.move_to(interaction.user.voice.channel)
-            else:
-                await interaction.user.voice.channel.connect(self_deaf=True, self_mute=True)
+            if interaction.guild.voice_client and interaction.guild.voice_client.channel != interaction.user.voice.channel:
+                await interaction.guild.voice_client.disconnect(force=False)
+            if not interaction.guild.voice_client:
+                await interaction.user.voice.channel.connect(self_mute=True, self_deaf=True)
             return True
         else:
             await interaction.followup.send("You must be in a voice channel to use this command")
@@ -182,41 +208,92 @@ async def skip(interaction: discord.Interaction):
     if interaction.guild.voice_client and interaction.guild.voice_client.is_playing():
         interaction.guild.voice_client.stop()
         if queue.empty():
-            await interaction.guild.voice_client.disconnect(force=True)
+            await interaction.guild.voice_client.disconnect(force=False)
         await interaction.response.send_message("Skipped")
     else:
         await interaction.response.send_message("Not playing anything")
 
 
+@bot.tree.command(description="Modify the playlist")
+async def playlist(interaction: discord.Interaction, action: str, url: str):
+    await interaction.response.defer()
+    pid = None
+    if 'bilibili' in url:
+        processed_url = url.split('/')[4]
+        match = re.search(r'\?p=\d+', url)
+        pid = int(match.group(0).split('=')[1]) if match else None
+        type = "0"
+    elif 'youtube' in url:
+        processed_url = url
+        type = "1"
+    else:
+        await interaction.followup.send("Unsupported URL")
+        return
+
+    if action == "add":
+        insert(processed_url, type, pid)
+        await interaction.followup.send(f"Added {processed_url} to playlist")
+    elif action == "delete":
+        delete(processed_url, pid)
+        await interaction.followup.send(f"Removed {processed_url} from playlist")
+    else:
+        await interaction.followup.send("Invalid action")
+
+
+@bot.tree.command(description="Play from playlist")
+async def playl(interaction: discord.Interaction, count: int = 5):
+    await interaction.response.defer()
+    if not await ensure_voice(interaction):
+        return
+    music_lst = random_music(count)
+    for music in music_lst:
+        if music.type == "0":
+            await enqueue_bilibili(interaction, music.url, music.pid)
+        elif music.type == "1":
+            await enqueue_ytb(interaction, music.url, False)
+
+
+
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    # print "ready" in the console when the bot is ready to work
     print("ready", flush=True)
 
 
 class Data:
-    def __init__(self, title, duration, image):
+    def __init__(self, type, title, duration, image, url):
+        """
+        :param type: 0 for bilibili, 1 for youtube
+        """
+        self.type = type
         self.title = title
         self.duration = duration
         self.image = image
+        self.url = url
 
     def __str__(self):
         return self.title + " " + str(self.duration) + " " + self.image
 
     @classmethod
-    def from_bili(cls, info):
-        return cls(info['title'], info['duration'], info['pic'])
+    def from_bili(cls, info, url, p, first=None):
+        if p:
+            if 'first_frame' in info:
+                return cls(0, info['part'], info['duration'], info['first_frame'], url)
+            else:
+                return cls(0, info['part'], info['duration'], first, url)
+        else:
+            return cls(0, info['title'], info['duration'], info['pic'], url)
 
     @classmethod
-    def from_yt(cls, info):
-        return cls(info['title'], info['duration'], info['thumbnail'])
+    def from_yt(cls, info, url):
+        return cls(1, info['title'], info['duration'], info['thumbnail'], url)
 
 
 async def main():
-    t1 = asyncio.create_task(_play())
-    await bot.start(TOKEN)
-    await t1
+    await asyncio.gather(
+        bot.start(TOKEN, ),
+        _play(),
+    )
 
 
 if __name__ == '__main__':
